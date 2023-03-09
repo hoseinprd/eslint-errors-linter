2a26f7c69f01ce2c008174f6635693afcf671661
Hossein Pourdavar <h.pourdavar@gmail.com>
2023-03-09 12:25:20 +0330

    checkArgsCase fn moved into utils

---
+++ src/utils/check-args-case.util.ts
@@ -0,0 +1,66 @@
+import { TSESTree } from "@typescript-eslint/utils";
+import { RuleContext } from "@typescript-eslint/utils/dist/ts-eslint";
+import {
+  isKebabCase,
+  isMacroCase,
+  isNoCase,
+  isSnakeCase,
+  isTrainCase,
+} from "naming-conventions-modeler";
+import { CaseType, Options } from "../rules";
+
+export const checkArgsCase = (
+  node: TSESTree.Node,
+  ctx: Readonly<RuleContext<"constantCase", Options>>,
+  type: CaseType,
+  pattern: string
+) => {
+  if (node.type !== "NewExpression" || node.callee.type !== "Identifier")
+    return;
+
+    console.log({ pattern, pass: new RegExp(pattern, "u").test(node.callee.name) })
+
+  if (!new RegExp(pattern, "u").test(node.callee.name)) return;
+
+  const errorArg = node.arguments[0];
+
+  if (errorArg?.type !== "Literal" || typeof errorArg.value !== "string")
+    return;
+
+  const argName = ctx.getSourceCode().getText(errorArg);
+
+  let isCaseMatched: boolean;
+  switch (type) {
+    case "MACRO_CASE":
+      isCaseMatched = isMacroCase(errorArg.value);
+      break;
+    case "Train-Case":
+      isCaseMatched = isTrainCase(errorArg.value);
+      break;
+    case "kebab-case":
+      console.log(type);
+      isCaseMatched = isKebabCase(errorArg.value);
+      break;
+    case "no case":
+      isCaseMatched = isNoCase(errorArg.value);
+      break;
+    case "snake_case":
+      isCaseMatched = isSnakeCase(errorArg.value);
+      break;
+    default:
+      isCaseMatched = false;
+  }
+
+  console.log(type, errorArg.value, { isCaseMatched });
+
+  if (!isCaseMatched) {
+    ctx.report({
+      node: errorArg,
+      messageId: "constantCase",
+      data: {
+        argName,
+        case: type,
+      },
+    });
+  }
+};
